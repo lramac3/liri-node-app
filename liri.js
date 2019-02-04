ae7c607d84b5dfcfa3544d54dbb89533e3b907c0
Linu Ramachandran <linuram@gmail.com>
2019-02-04 16:40:38 -0500

    updated liri.js

--- liri.js
+++ liri.js
@@ -4,3 +4,20 @@ const command = process.argv[2];
 const value = process.argv[3];
 // node liri.js concert-this <artist/band name here>
 
+//https://rest.bandsintown.com/artists/   /events?app_id=codingbootcamp
+
+function getMyBands(artist){
+// const url = 'https://rest.bandsintown.com/artists/'+artist+'/events?app_id=codingbootcamp';
+    const url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+    axios.get(url, {
+        params: {
+          ID: 12345
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
