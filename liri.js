98617670f4dba6165466728b57d581d759a712e8
Linu Ramachandran <linuram@gmail.com>
2019-02-04 16:14:34 -0500

    updated liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,16 @@
-require("dotenv").config();
+// require("dotenv").config();
+
+const inquirer = require('inquirer');
+
+const questions = [
+    {type: 'input', name: 'name', message:'What is your name'}
+    ]
+
+inquirer
+  .prompt(questions)
+    /* Pass your questions in here */
+  .then(answers => {
+    // Use user feedback for... whatever!!
+
+    console.log('Answers: ', answers)
+  });
